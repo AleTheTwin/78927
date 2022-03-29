3a9d26d7269f14caabdb8ab24d7efccbbc1d6f1c
Alejandro Sánchez <71783206+AleTheTwin@users.noreply.github.com>
2022-03-29 15:33:37 -0600

    Modificar, Buscar y Borrar Saludos

--- SaludosBd/src/main/java/mx/uv/t4is/SaludosBd/SaludosEndPoint.java
+++ SaludosBd/src/main/java/mx/uv/t4is/SaludosBd/SaludosEndPoint.java
@@ -4,6 +4,7 @@ import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.*;
@@ -38,14 +39,20 @@ public class SaludosEndPoint {
 
         return respuesta;
     }
-/*
+    
     @PayloadRoot(namespace = "https://t4is.uv.mx/saludos", localPart = "BuscarSaludosRequest")
     @ResponsePayload
     public BuscarSaludosResponse buscar() {
         BuscarSaludosResponse respuesta = new BuscarSaludosResponse();
-        for (Saludo saludo : saludos) {
+        Iterable<Saludadores> saludadores = isaludadores.findAll();
+
+        saludadores.forEach(isaludo -> {
+            Saludo saludo = new Saludo();
+            saludo.setId(isaludo.getId());
+            saludo.setNombre(isaludo.getNombre());
             respuesta.getSaludo().add(saludo);
-        }
+        });
+
         return respuesta;
     }
 
@@ -53,16 +60,21 @@ public class SaludosEndPoint {
     @ResponsePayload
     public ModificarSaludoResponse modificar(@RequestPayload ModificarSaludoRequest peticion) {
         ModificarSaludoResponse respuesta = new ModificarSaludoResponse();
+        
+        // Recuperar parametros de la petición 
         int id = peticion.getId();
         String nombre = peticion.getNombre();
 
-        Saludo newSaludo = new Saludo();
-        newSaludo.setId(id);
-        newSaludo.setNombre(nombre);
-
-        saludos.set(id, newSaludo);
-
-        respuesta.setRespuesta(true);
+        Optional<Saludadores> oSaludador = isaludadores.findById(id);
+        if(oSaludador.isPresent()) {
+            Saludadores saludador;
+            saludador = oSaludador.get();
+            saludador.setNombre(nombre);
+            isaludadores.save(saludador);
+            respuesta.setRespuesta(true);
+        } else {
+            respuesta.setRespuesta(false);
+        }
         return respuesta;
     }
 
@@ -71,24 +83,16 @@ public class SaludosEndPoint {
     public BorrarSaludoResponse borrar(@RequestPayload BorrarSaludoRequest peticion) {
         BorrarSaludoResponse respuesta = new BorrarSaludoResponse();
 
+        // Recuperar parametros de la petición 
         int id = peticion.getId();
 
-        int position = -1;
-
-        for(int i=0; i<saludos.size(); i++) {
-            if (saludos.get(i).getId() == id) {
-                position = i;
-            }
-        }
-
-        if(position != -1) {
-            saludos.remove(position);
+        Optional<Saludadores> oSaludador = isaludadores.findById(id);
+        if(oSaludador.isPresent()) {
+            isaludadores.deleteById(id);
             respuesta.setRespuesta(true);
         } else {
             respuesta.setRespuesta(false);
         }
-
         return respuesta;
     }
-    */
 }
